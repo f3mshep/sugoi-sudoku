44130d9dba0510f2c9548a0336d1b4c8e11d6b14
f3mshep <superbiscuit@gmail.com>
2018-01-09 15:58:54 -0800

    added yarn packages

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,27 @@
+class GameController < ApplicationController
+
+  def new
+    #handles a new game instance
+    #user will click easy, medium, or hard
+    #request would look something like  GET api/game/new/medium
+    #response will contain a valid board of appropriate difficulty
+  end
+
+  def create
+    # POST api/game with a JSON payload of the game.
+    # response contains an ID
+  end
+
+  def check
+    # POST api/game/check/id with JSON payload of game, will return true or false
+  end
+
+  def update
+    #POST api/game/id with a JSON payload of the game
+  end
+
+  def show
+    #GET api/game/id
+  end
+
+end
