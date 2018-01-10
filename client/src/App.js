43b9e0209630b5464f0e17c7d4e222d31cff47bc
f3mshep <superbiscuit@gmail.com>
2018-01-09 16:41:17 -0800

    added some boilerplate react/redux code

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
+
+  createBoard = () => {
+    for (let index = 0; index < 9; index++) {
+
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return
+
+    ;
   }
 }
 
---
+++ client/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import { routerMiddleware } from 'react-route-redux'
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+export default store
