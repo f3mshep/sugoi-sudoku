fb4c17a735dfe9fcd3fcc91af7c891f30b8b7ddf
Alexandra Wright <superbiscuit@gmail.com>
2018-01-16 22:35:26 -0800

    added defect to todo list

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
-import "bootstrap/dist/css/bootswatch.css";
+import "./styles/bootswatch.min.css";
 import "./styles/sudoku.css"
 import "font-awesome/css/font-awesome.css";
 
